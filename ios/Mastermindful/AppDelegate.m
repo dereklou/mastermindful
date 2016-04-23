9a8d6501bf44f98b3ff2f9d21179e3a2b4453bcb
Naseem Alnaji <nalnaji@berkeley.edu>
2016-04-23 15:21:17 -0700

    launchscreen

--- ios/Mastermindful/AppDelegate.m
+++ ios/Mastermindful/AppDelegate.m
@@ -15,6 +15,17 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
+  for (NSString* family in [UIFont familyNames])
+  {
+    NSLog(@"%@", family);
+    
+    for (NSString* name in [UIFont fontNamesForFamilyName: family])
+    {
+      NSLog(@"  %@", name);
+    }
+  }
+
   NSURL *jsCodeLocation;
 
   /**
